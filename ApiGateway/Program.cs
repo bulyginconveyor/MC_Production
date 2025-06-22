771e66a57bf62bc942559d66ee52a881d7964380
Anavoviy <vovan200512345@mail.ru>
2025-06-23 07:35:25 +1000

    update

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,6 +1,23 @@
+using ApiGateway.jwt_authentification;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Path.Combine(builder.Environment.ContentRootPath))
+    .AddOcelot("configs", builder.Environment) 
+    .AddEnvironmentVariables();
+
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddJwtAuthentification();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
---
+++ ApiGateway/jwt_authentification/JwtTokenHandler.cs
@@ -0,0 +1,71 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ApiGateway.jwt_authentification;
+
+public class JwtTokenHandler(string keySecret)
+{
+    public string jwtSecretKey = keySecret;
+    private int jwtAccessTokenValidityMins = 12;
+    private int jwtRefreshTokenValidityDays = 91;
+    
+    public (string, DateTime) GenerateAccessJwtToken(string role, Guid id)
+    {
+        var tokenExpiryTimeStamp = DateTime.UtcNow.AddMinutes(jwtAccessTokenValidityMins);
+        var tokenKey = Encoding.ASCII.GetBytes(jwtSecretKey);
+        var claimsIdentity = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim(ClaimTypes.Role, role),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim("uid", id.ToString())
+        });
+
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(tokenKey),
+            SecurityAlgorithms.HmacSha256Signature);
+
+        var securityTokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = claimsIdentity,
+            Expires = tokenExpiryTimeStamp,
+            SigningCredentials = signingCredentials
+        };
+
+        var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        var securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+        var token = jwtSecurityTokenHandler.WriteToken(securityToken);
+
+        return (token, tokenExpiryTimeStamp);
+    }
+    
+    public (string, DateTime) GenerateRefreshJwtToken(string role, Guid id)
+    {
+        var tokenExpiryTimeStamp = DateTime.UtcNow.AddDays(jwtRefreshTokenValidityDays);
+        var tokenKey = Encoding.ASCII.GetBytes(jwtSecretKey);
+        var claimsIdentity = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim(ClaimTypes.Role, role),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim("uid", id.ToString())
+        });
+
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(tokenKey),
+            SecurityAlgorithms.HmacSha256Signature);
+
+        var securityTokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = claimsIdentity,
+            Expires = tokenExpiryTimeStamp,
+            SigningCredentials = signingCredentials
+        };
+
+        var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        var securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+        var token = jwtSecurityTokenHandler.WriteToken(securityToken);
+
+        return (token, tokenExpiryTimeStamp);
+    }
+}
